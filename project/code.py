a7c0c5336f88d495e3cbaa9d7522f578cfaa2546
Muhammad Ehtisham <56553952+MEhtisham8217@users.noreply.github.com>
2020-04-28 22:34:24 +0500

    Update code.py

--- project/code.py
+++ project/code.py
@@ -1,8 +1,6 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('sample.jpeg',0)
-
 def gaussian_blur(img, kernel_size):
     return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
   
@@ -10,16 +8,30 @@ def canny(img, low_threshold, high_threshold):
     return cv2.Canny(img, low_threshold, high_threshold)
 
 
-# apply gaussian blur
-kernelSize = 5
-gaussianBlur = gaussian_blur(img, kernelSize)
+cap = cv2.VideoCapture("road.mp4")
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
 
-# canny
-minThreshold = 100
-maxThreshold = 200
-edgeDetectedImage = canny(gaussianBlur, minThreshold, maxThreshold)
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # apply gaussian blur
+    kernelSize = 5
+    gaussianBlur = gaussian_blur(gray, kernelSize)
+    # canny
+    minThreshold = 100
+    maxThreshold = 200
+    edgeDetectedImage = canny(gaussianBlur, minThreshold, maxThreshold)
+        
+    # Display the resulting frame
+    cv2.imshow('frame', edgeDetectedImage)
+    if cv2.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()    
 
-#display
-cv2.imshow('image',edgeDetectedImage)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
